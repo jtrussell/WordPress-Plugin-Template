39ebf398dbca20dc4493398ee401f343f6f3b917
Justin Russell <jus.russell@gmail.com>
2012-01-09 09:54:22 -0500

    Started new library file for io functions

---
+++ lib/lib-io.php
@@ -0,0 +1,28 @@
+<?php
+// -----------------------------------------------------
+// Class:	template_io
+// Author:	Justin Russell
+// Date:	
+// Purpose:	Encapsulate functionality related to
+// 			generic input and output
+// -----------------------------------------------------
+
+class template_io
+{
+
+	public static function log_folder() {
+		return dirname(__file__) . "/../logs";
+	}
+
+	public static function log($msg, $log_file_name = "debug.txt") {
+		$log_file = self::log_folder . "/" .$log_file_name;
+		$handle = fopen($log_file, "a");
+		fwrite($handle, date(DATE_RFC822, time()) . "\t" . $msg . "\r\n");
+		fclose($handle);
+	}
+
+}
+
+// -----------------------------------------------------
+// Closing php tag omitted
+// -----------------------------------------------------
