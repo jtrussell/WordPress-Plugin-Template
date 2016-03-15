e199a31ca110c8de947da89a113c35eda296057f
jtrussell <jus.russell@gmail.com>
2016-03-15 15:25:11 -0400

    style: Tabs -> spaces

--- form/options-form.php
+++ form/options-form.php
@@ -1,30 +1,30 @@
 <div class="wrap">
-	<div id="icon-options-general" class="icon32"></div>
-	<h2><?php echo template_wp::$plugin_name; ?> Settings</h2>
-	<form action="options.php" method="post">
-		<?php settings_fields(template_wp::$option_group); ?>
-		<table class="form-table">
-			<tbody>
-				<!-- My Option -->
-				<?php
-					$option	= "my_option";
-					$label	= "My Option";
-					$title	= "title text";
-					$name 	= template_wp::option_name($option);
-					$id		= template_wp::option_name($option);
-					$value	= template_wp::get_option($option);
-				?>
-				<tr valign="top" title="<?php echo $title ?>">
-					<th>
-						<label for="<?php echo $id; ?>"><?php echo $label ?></label>
-					</th>
-					<td>
-						<input name="<?php echo $name; ?>" value="<?php echo $value; ?>" id="<?php echo $id ?>" type="text" class="regular-text" />
-					</td>
-				</tr><!-- End My Option -->
+  <div id="icon-options-general" class="icon32"></div>
+  <h2><?php echo template_wp::$plugin_name; ?> Settings</h2>
+  <form action="options.php" method="post">
+    <?php settings_fields(template_wp::$option_group); ?>
+    <table class="form-table">
+      <tbody>
+        <!-- My Option -->
+        <?php
+          $option = "my_option";
+          $label = "My Option";
+          $title = "title text";
+          $name = template_wp::option_name($option);
+          $id = template_wp::option_name($option);
+          $value = template_wp::get_option($option);
+        ?>
+        <tr valign="top" title="<?php echo $title ?>">
+          <th>
+            <label for="<?php echo $id; ?>"><?php echo $label ?></label>
+          </th>
+          <td>
+            <input name="<?php echo $name; ?>" value="<?php echo $value; ?>" id="<?php echo $id ?>" type="text" class="regular-text" />
+          </td>
+        </tr><!-- End My Option -->
 
-			</tbody>
-		</table>
-		<p class="submit"><input type="submit" class="button-primary" name="submit" value="Save Changes" /></p>
-	</form>
-</div><!-- End wrap -->
+      </tbody>
+    </table>
+    <p class="submit"><input type="submit" class="button-primary" name="submit" value="Save Changes" /></p>
+  </form>
+</div>
--- wp-plugin-template.php
+++ wp-plugin-template.php
@@ -8,42 +8,28 @@ Author: Justin Russell
 Author URI: http://jrussell.me
 */
 
-// -----------------------------------------------------
 // Setup some constants
-// -----------------------------------------------------
 $this_dir = dirname(__file__);
 $lib_dir = $this_dir . "/lib";
 
-
-// -----------------------------------------------------
 // Include our library files
-// -----------------------------------------------------
 if ($handle = opendir($lib_dir)) {
-	while (false !== ($file = readdir($handle))) {
-		// Don't include hidden files or directories
-		$file_path = $lib_dir . "/" . $file;
-		if (preg_match("/(lib|class)-[\w-]+\.php/", $file_path) && is_file($file_path)) { 
-			// Do Something with file
-			include_once($file_path);
-		}
-	}
-	closedir($handle);
+  while (false !== ($file = readdir($handle))) {
+    // Don't include hidden files or directories
+    $file_path = $lib_dir . "/" . $file;
+    if (preg_match("/(lib|class)-[\w-]+\.php/", $file_path) && is_file($file_path)) { 
+      include_once($file_path);
+    }
+  }
+  closedir($handle);
 }
 
 
-// -----------------------------------------------------
 // Register our options menu item and options page
-// -----------------------------------------------------
 add_action("admin_menu", "template_ui::print_plugin_menu");
 
-
-// -----------------------------------------------------
 // Activation, Deactivation, and Install routines
-// -----------------------------------------------------
 register_activation_hook(__FILE__, "template_wp::set_options");
 register_deactivation_hook(__FILE__, "template_wp::unset_options");
 add_action("admin_init", "template_wp::register_settings");
 
-// -----------------------------------------------------
-// Closing php tag omitted
-// -----------------------------------------------------
